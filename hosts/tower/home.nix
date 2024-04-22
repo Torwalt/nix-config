cbb0b9bda092dc8225c8dcde562d6d90d96a8026
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-04-22 20:22:03 +0200

    Reuse base.nix in tower setup

--- hosts/tower/home.nix
+++ hosts/tower/home.nix
@@ -1,7 +1,9 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
+    ../../modules/base.nix
+
     ../../modules/nvim/nvim.nix
 
     ../../modules/cli/all.nix
@@ -15,97 +17,27 @@
     ../../modules/wm/hyprland/gammastep.nix
   ];
 
-  nixpkgs = {
-    overlays = [
-      (final: prev: {
-        vimPlugins = prev.vimPlugins // {
-          vim-delve-nvim = prev.vimUtils.buildVimPlugin {
-            name = "vim-delve";
-            src = inputs.plugin-vim-delve;
-          };
-        };
-      })
-    ];
-
-    # Configure your nixpkgs instance
-    config = {
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
-  };
-
-  programs.home-manager.enable = true;
-
-  fonts.fontconfig.enable = true;
-
   home = {
     username = "ada";
     homeDirectory = "/home/ada";
 
     packages = with pkgs; [
-      gcc
-      go
-      jq
-      keepassxc
-      python3
-      zsh-vi-mode
-      ripgrep
-      delve
-      gnumake
-      nixfmt
-      firefox
-      pavucontrol
-      telegram-desktop
-      ffmpeg
-      spotify
-      chromium
-      htop
-      btop
-
       # Monitor config
       nwg-displays
       wlr-randr
-
-      # Screenshots
-      grim
-      slurp
-      wl-clipboard
-      swappy
-
-      # Fonts
-      fira-code
-      fira-code-symbols
-      font-awesome
-      liberation_ttf
-      mplus-outline-fonts.githubRelease
-      nerdfonts
-      noto-fonts
-      noto-fonts-emoji
-      proggyfonts
     ];
 
     sessionVariables = {
-      EDITOR = "nvim";
-      VISUAL = "nvim";
       WLR_NO_HARDWARE_CURSORS = 1;
     };
-
-    sessionPath = [ "$HOME/go/bin" ];
   };
 
   wayland.windowManager.hyprland = {
     settings = {
-
       monitor = [
         "DP-1,2560x1440@59.951,0x0,1.0,bitdepth,10"
         "HDMI-A-3,2560x1440@99.945999,2560x0,1.0,bitdepth,10"
       ];
     };
   };
-
-  systemd.user.startServices = "sd-switch";
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.11";
 }
