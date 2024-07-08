efcd162ea539bde70b0378d4740d9d0c390b9890
Alexander Dadiani <dadiani@talon.one>
2024-07-08 14:25:48 +0200

    config git for work and setup monitor

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, lib, ... }:
 
 {
   imports = [
@@ -31,6 +31,7 @@
       wlr-randr
 
       libreoffice
+      slack
     ];
 
     sessionVariables = { WLR_NO_HARDWARE_CURSORS = 1; };
@@ -38,17 +39,21 @@
 
   wayland.windowManager.hyprland = {
     settings = {
-      # monitor = [
-      #   "DP-1,2560x1440@59.951,0x0,1.0,bitdepth,10"
-      #   "HDMI-A-3,2560x1440@99.945999,2560x0,1.0,bitdepth,10"
-      #   "Unknown-1,disabled"
-      # ];
+      monitor = [
+        "eDP-1,1920x1200@59.95000,0x0,1.0,bitdepth,10"
+        "DP-3,2560x1440@59.95Hz,2560x0,1.0,bitdepth,10"
+      ];
     };
   };
 
   programs.zsh.shellAliases = {
     sysswitch = "sudo nixos-rebuild --flake .#workSys switch";
     homeswitch = "home-manager switch --flake .#workHome";
+  };
+
+
+  programs.git = {
+    userEmail = lib.mkForce "dadiani@talon.one";
   };
 
   colorScheme = inputs.nix-colors.colorSchemes.tokyo-city-dark;
