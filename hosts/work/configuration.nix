3c36fc99f0f8a4398a14b50c2bec325b98f5bd0f
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-07-08 12:47:13 +0200

    setup new new work station

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -1,10 +1,15 @@
 { lib, ... }: {
   imports = [
+    ./hardware-configuration.nix
     ../../modules/stylix/default.nix
     ../../modules/system/default.nix
     ../../modules/system/printing.nix
     ../../modules/system/hyprland.nix
   ];
+
+  boot.initrd.luks.devices."luks-7467625b-5d73-418d-9dcc-ade392ca09c9".device = "/dev/disk/by-uuid/7467625b-5d73-418d-9dcc-ade392ca09c9";
+
+system.stateVersion = "24.05";
 
   boot.loader = {
     systemd-boot.enable = lib.mkForce false;
