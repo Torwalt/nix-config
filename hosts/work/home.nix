73585bd6b7f1c9c409ced9c2ed457a76cf20d202
Alexander Dadiani <dadiani@talon.one>
2024-07-19 09:39:39 +0200

    gcloud with extra components

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -34,7 +34,7 @@ in {
 
       libreoffice
       slack
-      google-cloud-sdk
+      (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
       beekeeper-studio
       go-task
       postgresql
