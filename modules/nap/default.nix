87985060b90a125f3b4a0334a4d435ec1589af19
Alexander Dadiani <dadiani@talon.one>
2024-08-13 13:07:56 +0200

    fix nap config path

--- modules/nap/default.nix
+++ modules/nap/default.nix
@@ -6,7 +6,12 @@
       target = "/home/ada/.config/nap/config.yaml";
     };
 
-    sessionVariables = { NAP_CONFIG = "home/ada/.config/nap/config.yaml"; };
+    sessionVariables = {
+      NAP_CONFIG = "/home/ada/.config/nap/config.yaml";
+      XDG_CONFIG_HOME = "/home/ada/.config";
+      # This should not be required, but better have it to make sure.
+      NAP_HOME = "/home/ada/repos/notes/snippets";
+    };
   };
 
 }
