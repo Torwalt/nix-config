74e87287bf553738139864e3843a9695130a6502
Alexander Dadiani <dadiani@talon.one>
2024-09-30 11:24:15 +0200

    AFUERA printing for now

--- modules/system/printing.nix
+++ modules/system/printing.nix
@@ -3,12 +3,17 @@
   services.printing = {
     enable = true;
     drivers = with pkgs; [ cnijfilter2 ];
+
+    # Due to CUPS CVE
+    browsed.enable = false;
+    browsing = false;
+    startWhenNeeded = false;
   };
   # Enable Printer autodiscovery.
   services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
+    enable = false;
+    nssmdns4 = false;
+    openFirewall = false;
   };
   # Enable scanning.
   hardware.sane = {
