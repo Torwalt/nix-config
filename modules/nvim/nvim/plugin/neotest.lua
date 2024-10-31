bfa93925add2f555722cf74b45d47c415da5c33f
Alexander Dadiani <dadiani@talon.one>
2024-10-31 10:33:40 +0100

    Fix neotest for go by adding tags

--- modules/nvim/nvim/plugin/neotest.lua
+++ modules/nvim/nvim/plugin/neotest.lua
@@ -1,22 +1,20 @@
 local goconf = {
-  go_test_args = {
-    "-v",
-  },
-  runner = "gotestsum"
+    go_test_args = {"-v", "-tags=integration,clickhouse"},
+    go_list_args = {"-tags=integration,clickhouse"},
+    dap_go_opts = {delve = {build_flags = {"-tags=integration,clickhouse"}}},
+    runner = "gotestsum"
 }
 
 require("neotest").setup({
     adapters = {
-      require("neotest-golang")(goconf),
-      require('rustaceanvim.neotest'),
+        require("neotest-golang")(goconf), require('rustaceanvim.neotest')
     }
 })
 
 local function run_test_and_open_debug()
-    require('neotest').run.run({ suite = false, strategy = 'dap' })
+    require('neotest').run.run({suite = false, strategy = 'dap'})
     require('dap').repl.toggle(nil, 'vsplit')
     vim.cmd("wincmd p")
 end
 
 nmap("<leader>dt", run_test_and_open_debug)
-
