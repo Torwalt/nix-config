f739672a96a75c9512d25044a08771d51edf8ccd
Alexander Dadiani <alexdadiani1994@gmail.com>
2024-11-12 21:00:10 +0100

    Fix markdown code block syntax highlighting

--- modules/nvim/nvim/plugin/nvim-treesitter.lua
+++ modules/nvim/nvim/plugin/nvim-treesitter.lua
@@ -1,27 +1,23 @@
 local api = vim.api
 
 local function ts_disable(lang, bufnr)
-    local disabled_langs = { "yaml", "json" }
+    local disabled_langs = {"yaml", "json"}
     local line_count_exceeds = api.nvim_buf_line_count(bufnr) > 5000
 
     for _, l in ipairs(disabled_langs) do
-        if l == lang then
-            return line_count_exceeds
-        end
+        if l == lang then return line_count_exceeds end
     end
 
     return false
 end
 
-require 'nvim-treesitter.configs'.setup {
-  ensure_installed = {},
-  auto_install = false,
-  highlight = {
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {},
+    auto_install = false,
+    highlight = {
         enable = true,
-        disable = ts_disable
+        disable = ts_disable,
+        additional_vim_regex_highlighting = false
     },
-  indent = {
-        enable = true,
-        disable = ts_disable
-    }
+    indent = {enable = true, disable = ts_disable}
 }
