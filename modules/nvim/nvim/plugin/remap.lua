77a158f07a471114a19157d2f6c2ea01c3d2237a
Alexander Dadiani <dadiani@talon.one>
2025-02-19 14:33:35 +0100

    add binding to toggle window maximization
    - remove unnecessary telekasten bindings

--- modules/nvim/nvim/plugin/remap.lua
+++ modules/nvim/nvim/plugin/remap.lua
@@ -6,3 +6,20 @@ nmap("<Esc>", "<C-\\><C-n>")
 nmap("<leader>jf", ":%!jq .<cr>")
 nmap("<leader>s", ":update<cr>")
 
+local window_maximized = false
+
+-- Function to toggle window maximization
+function toggle_window_maximize()
+  if window_maximized then
+    -- Restore equal sizing
+    vim.cmd('wincmd =')
+    window_maximized = false
+  else
+    -- Maximize current window
+    vim.cmd('wincmd _')
+    vim.cmd('wincmd |')
+    window_maximized = true
+  end
+end
+
+nmap('<leader>zz', toggle_window_maximize)
--- modules/nvim/nvim/plugin/telekasten.lua
+++ modules/nvim/nvim/plugin/telekasten.lua
@@ -9,13 +9,3 @@ telekasten.setup({
 -- Launch panel if nothing is typed after <leader>z
 nmap("<leader>Z", function() telekasten.panel() end)
 
--- Most used functions
-nmap("<leader>zf", function() telekasten.find_notes() end)
-nmap("<leader>zg", function() telekasten.search_notes() end)
-nmap("<leader>zd", function() telekasten.goto_today() end)
-nmap("<leader>zz", function() telekasten.follow_link() end)
-nmap("<leader>zn", function() telekasten.new_note() end)
-nmap("<leader>zc", function() telekasten.show_calendar() end)
-nmap("<leader>zb", function() telekasten.show_backlinks() end)
-nmap("<leader>zI", function() telekasten.insert_img_link() end)
-nmap("<leader>zL", function() telekasten.insert_link() end)
