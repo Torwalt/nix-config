88c873427bb49a292b2c5d0bc6f39e2cde03cce9
Alexander Dadiani <alexdadiani1994@gmail.com>
2025-02-26 22:12:52 +0100

    cleanup work for new work

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -1,15 +1,11 @@
-{ lib, pkgs, ... }: {
+{ lib, ... }: {
   imports = [
     ./hardware-configuration.nix
     ../../modules/stylix/default.nix
     ../../modules/system/default.nix
     ../../modules/system/printing.nix
     ../../modules/system/hyprland.nix
-    # ../../modules/vanta/module.nix
   ];
-
-  boot.initrd.luks.devices."luks-7467625b-5d73-418d-9dcc-ade392ca09c9".device =
-    "/dev/disk/by-uuid/7467625b-5d73-418d-9dcc-ade392ca09c9";
 
   system.stateVersion = "24.11";
 
@@ -22,105 +18,9 @@
     timeout = 30;
   };
 
-  services.thermald.enable = false;
-  services.power-profiles-daemon.enable = false;
-
   powerManagement.cpuFreqGovernor = "performance";
 
-  hardware = {
-    graphics = {
-      enable32Bit = true;
-      extraPackages = with pkgs; [
-        intel-media-driver
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-    };
-  };
-
-  # services.resolved.enable = true;
-
-  # services.openvpn.servers = {
-  #   officeVPN = {
-  #     config = "config /home/ada/client.ovpn.bkp";
-  #     updateResolvConf = true;
-  #     autoStart = false;
-  #   };
-  # };
-  # networking.networkmanager.dns = "systemd-resolved";
-
-  # This is required in order to pull some moby images as they have high uid/guid set.
-  # See https://github.com/moby/moby/issues/43576#issuecomment-1136056638.
-  users.users.ada = {
-    subUidRanges = [
-      {
-        count = 65536;
-        startUid = 100000;
-      }
-      {
-        count = 1000000;
-        startUid = 1000000;
-      }
-    ];
-    subGidRanges = [
-      {
-        count = 65536;
-        startGid = 100000;
-      }
-      {
-        count = 1000000;
-        startGid = 1000000;
-      }
-    ];
-  };
-
-  # Increase ulimit for procs because of a docker image setting for work.
-  security.pam.loginLimits = [{
-    domain = "*";
-    type = "-";
-    item = "nproc";
-    value = "65536";
-  }];
-
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    LIBVA_DRIVER_NAME = "iHD"; # Force intel-media-driver
-  };
+  environment.sessionVariables = { NIXOS_OZONE_WL = "1"; };
 
   nixpkgs.config.allowUnfree = true;
-
-  # Required for vanta-agent.
-  services.osquery = {
-    enable = false;
-    flags = {
-      extensions_autoload = ''
-        /var/vanta/osquery-vanta.ext
-      '';
-    };
-
-    settings = {
-      options = {
-        logger_path = "/var/log/osquery";
-        disable_logging = false;
-        schedule_splay_percent = 10;
-      };
-
-      schedule = {
-        system_info = {
-          query =
-            "SELECT hostname, cpu_brand, physical_memory FROM system_info;";
-          interval = 3600;
-        };
-      };
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    distrobox
-    dpkg
-    osquery
-    lm_sensors
-    dmidecode
-    libsmbios
-  ];
 }
--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -36,64 +36,12 @@ in {
       # Monitor config
       nwg-displays
       wlr-randr
-      openvpn
 
       libreoffice
-      slack
-      (google-cloud-sdk.withExtraComponents
-        [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
-      beekeeper-studio
       go-task
-      postgresql
-      golangci-lint
-      clickhouse
-      yarn
-
-      kubectl
-      kubectx
     ];
 
     sessionVariables = { WLR_NO_HARDWARE_CURSORS = 1; };
-  };
-
-  wayland.windowManager.hyprland = {
-    settings = {
-      monitor = [
-        "eDP-1,1920x1200@59.95000,0x0,1.0"
-        "DP-3,2560x1440@59.95Hz,2560x0,1.0"
-      ];
-
-      workspace = [
-        "1, monitor:DP-3"
-        "3, monitor:DP-3"
-        "4, monitor:DP-3"
-        "5, monitor:DP-3"
-        "6, monitor:DP-3"
-        "7, monitor:DP-3"
-
-        "2, monitor:eDP-1"
-      ];
-
-      windowrulev2 = [
-        "workspace 1,class:^(kitty)$"
-        "workspace 2,title:^(Spotify Premium)$"
-        "workspace 3,class:^(firefox)$"
-        "workspace 4,class:^(Slack)$"
-        "workspace 5,class:^(beekeeper-studio)$"
-        "workspace 6,class:^(org.keepassxc.KeePassXC)$"
-      ];
-
-      exec-once = [
-        "kitty"
-        "firefox"
-        "slack"
-        "beekeeper-studio --use-gl=desktop"
-        "keepassxc"
-        "spotify"
-        "xrandr --output DP-1 --primary"
-      ];
-    };
-
   };
 
   programs.zsh.shellAliases = {
@@ -101,7 +49,7 @@ in {
     homeswitch = "home-manager switch --flake .#workHome";
   };
 
-  programs.git = { userEmail = lib.mkForce "dadiani@talon.one"; };
+  programs.git = { userEmail = lib.mkForce "alexdadiani1994@gmail.com"; };
 
   colorScheme = inputs.nix-colors.colorSchemes.tokyo-city-dark;
 }
