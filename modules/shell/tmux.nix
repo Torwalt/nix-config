afea08aa4cdae6d6e9fdaa6cf6619fd20472098f
Alexander Dadiani <alexdadiani1994@gmail.com>
2025-06-04 12:15:18 +0200

    Add tmux-fzf

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -5,11 +5,16 @@
     terminal = "screen-256color";
     historyLimit = 100000;
     keyMode = "vi";
+    extraConfig = ''
+      # session fzf to switch to
+      bind-key f run-shell -b "${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf/scripts/session.sh switch"
+    '';
 
     plugins = with pkgs; [
       tmuxPlugins.sensible
       tmuxPlugins.resurrect
       tmuxPlugins.yank
+      tmuxPlugins.tmux-fzf
     ];
   };
 }
