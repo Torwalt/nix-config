a2f672bbfd177a1ab47213505e402553fef0b1a8
Alexander Dadiani <alexdadiani1994@gmail.com>
2025-06-09 14:30:11 +0200

    Enable nix-direnv
    so that `use flake` works

--- modules/direnv/default.nix
+++ modules/direnv/default.nix
@@ -1,9 +1,10 @@
 { lib, ... }: {
-    programs.direnv = {
-        enable = true;
-        enableZshIntegration = true;
-        # env vars need to be set like
-        # `export VAR=x`
-        config = lib.importTOML ./config.toml;
-    };
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    # env vars need to be set like
+    # `export VAR=x`
+    config = lib.importTOML ./config.toml;
+    nix-direnv.enable = true;
+  };
 }
