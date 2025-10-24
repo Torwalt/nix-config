32bf1952ada856206047be5ee100f40ffc921d1f
Alexander Dadiani <alexdadiani1994@gmail.com>
2025-10-24 16:01:55 +0200

    prep asus

--- hosts/asus/configuration.nix
+++ hosts/asus/configuration.nix
@@ -5,11 +5,14 @@
     ./hardware-configuration.nix
     ../../modules/stylix/default.nix
     ../../modules/system/default.nix
-    ../../modules/system/printing.nix
     ../../modules/system/hyprland.nix
+    ../../modules/system/greetd/default.nix
+    ../../modules/system/fonts.nix
   ];
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "25.05";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  powerManagement.cpuFreqGovernor = "performance";
+
+  nixpkgs.config.allowUnfree = true;
 }
--- hosts/asus/home.nix
+++ hosts/asus/home.nix
@@ -2,14 +2,22 @@
   imports = [
     ../../modules/base.nix
     ../../modules/stylix/home.nix
+    ../../modules/stylix/default.nix
     ../../modules/rofi/default.nix
+    ../../modules/nap/default.nix
+    ../../modules/direnv/default.nix
+    ../../modules/delve/default.nix
+    ../../modules/lazydocker/default.nix
+    ../../modules/timewarrior/default.nix
 
     ../../modules/nvim/nvim.nix
 
     ../../modules/cli/all.nix
+    ../../modules/go/default.nix
 
     ../../modules/shell/shell.nix
     ../../modules/shell/tmux.nix
+    ../../modules/shell/ssh.nix
 
     ../../modules/wm/hyprland/hyprland.nix
     ../../modules/wm/hyprland/waybar.nix
