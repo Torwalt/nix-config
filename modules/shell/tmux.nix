a4b45ea77d8ce2bb46e7addcdec68d863b0ab94b
Alexander Dadiani <alexdadiani1994@gmail.com>
2026-07-30 14:51:22 +0200

    Fix opening hyperlink when having relative numbers
    - tmux' relative numbers shift the position of the hyperlink set by
      delta by the column amount the digits take up
    - adjust open-nvim-url to scan the whole row for the first hyperlink
      found, rather than looking only under the cursor

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -5,59 +5,103 @@ let
     runtimeInputs = with pkgs; [
       neovim
       python3
+      pkgs-unstable.tmux
     ];
 
     text = ''
             socket="''${XDG_RUNTIME_DIR:?}/nvim-main.sock"
-            url="''${1:-}"
 
-            if [[ -z "$url" ]]; then
-              exit 0
-            fi
+            open_url() {
+              local url="''${1:-}"
 
-            if [[ ! -S "$socket" ]]; then
-              printf 'Neovim socket not found: %s\n' "$socket" >&2
-              exit 1
-            fi
+              if [[ -z "$url" ]]; then
+                exit 0
+              fi
 
-            case "$url" in
-              file://*) ;;
-              *)
-                printf 'Unsupported URL: %s\n' "$url" >&2
+              if [[ ! -S "$socket" ]]; then
+                printf 'Neovim socket not found: %s\n' "$socket" >&2
                 exit 1
-                ;;
-            esac
+              fi
 
-            target="''${url#file://}"
+              case "$url" in
+                file://*) ;;
+                *)
+                  printf 'Unsupported URL: %s\n' "$url" >&2
+                  exit 1
+                  ;;
+              esac
 
-            if [[ "$target" == *"#"* ]]; then
-              path="''${target%%#*}"
-              line="''${target##*#}"
-            else
-              path="$target"
-              line=1
-            fi
+              target="''${url#file://}"
 
-            if [[ ! "$line" =~ ^[0-9]+$ ]]; then
-              line=1
-            fi
+              if [[ "$target" == *"#"* ]]; then
+                path="''${target%%#*}"
+                line="''${target##*#}"
+              else
+                path="$target"
+                line=1
+              fi
 
-            path="$(
-              python3 - "$path" <<'PY'
+              if [[ ! "$line" =~ ^[0-9]+$ ]]; then
+                line=1
+              fi
+
+              path="$(
+                python3 - "$path" <<'PY'
       import sys
       import urllib.parse
 
       print(urllib.parse.unquote(sys.argv[1]))
       PY
-            )"
+              )"
 
-            if [[ ! -e "$path" ]]; then
-              printf 'File does not exist: %s\n' "$path" >&2
-              exit 1
-            fi
+              if [[ ! -e "$path" ]]; then
+                printf 'File does not exist: %s\n' "$path" >&2
+                exit 1
+              fi
 
-            nvim --server "$socket" --remote "$path"
-            nvim --server "$socket" --remote-send "<C-\\><C-N>:''${line}<CR>zz"
+              nvim --server "$socket" --remote "$path"
+              nvim --server "$socket" --remote-send "<C-\\><C-N>:''${line}<CR>zz"
+            }
+
+            open_tmux_copy_row() {
+              local pane="''${1:?}"
+              local original_x="''${2:-0}"
+              local width="''${3:-0}"
+              local url=""
+
+              if [[ ! "$original_x" =~ ^[0-9]+$ ]]; then
+                original_x=0
+              fi
+
+              if [[ ! "$width" =~ ^[0-9]+$ ]] || (( width == 0 )); then
+                width="$(tmux display-message -p -t "$pane" '#{pane_width}')"
+              fi
+
+              tmux send-keys -t "$pane" -X start-of-line
+
+              for ((x = 0; x < width; x++)); do
+                url="$(tmux display-message -p -t "$pane" '#{copy_cursor_hyperlink}')"
+                if [[ -n "$url" ]]; then
+                  break
+                fi
+
+                tmux send-keys -t "$pane" -X cursor-right
+              done
+
+              tmux send-keys -t "$pane" -X start-of-line
+              if (( original_x > 0 )); then
+                tmux send-keys -t "$pane" -N "$original_x" -X cursor-right
+              fi
+
+              if [[ -z "$url" ]]; then
+                tmux display-message -t "$pane" "No hyperlink on row"
+                exit 0
+              fi
+
+              open_url "$url"
+            }
+
+            open_tmux_copy_row "$@"
     '';
   };
 in
@@ -78,9 +122,7 @@ in
       bind-key f run-shell -b "${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf/scripts/session.sh switch"
 
       bind-key -T copy-mode-vi o \
-        if-shell -F '#{!=:#{copy_cursor_hyperlink},}' \
-          'run-shell "${openNvimUrl}/bin/open-nvim-url \"#{copy_cursor_hyperlink}\""' \
-          'display-message "No hyperlink under cursor"'
+        run-shell "${openNvimUrl}/bin/open-nvim-url '#{pane_id}' '#{copy_cursor_x}' '#{pane_width}'"
 
       set -g allow-passthrough on
       set -g copy-mode-line-numbers relative
